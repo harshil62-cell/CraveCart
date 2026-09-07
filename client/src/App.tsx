6975499652dd0b52ccf1efa435bfa60e12e516f9
harshil62-cell <harshil2461@gmail.com>
2026-09-07 23:44:03 +0530

    feat: initial commit - project initialization

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center cursor-pointer">
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
